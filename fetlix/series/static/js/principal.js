3576a264bdb581be7b2b976a9c9df8b9ff81f01f
Daniel Castellote <daniel.castellote.b@gmail.com>
2022-05-11 22:12:52 +0200

    Cargando datos de la API

--- fetlix/series/static/js/principal.js
+++ fetlix/series/static/js/principal.js
@@ -1,14 +1,48 @@
 const API_URL ="https://jsonplaceholder.typicode.com";
-const HTMLResponse = document.querySelector("#datos");
-const tpl=document.createElement;
 
 fetch(`${API_URL}/users`)
     .then((response)=>response.json())
-    /*.then((response)=>{
+    .then((response)=>{
         response.forEach((user)=>{
-            let nombre = document.getElementById('nombrecito')
-            nombre.append(`${user.name}`)
-            HTMLResponse.appendChild(nombre)
-        })      
-    })
-    .then((response) =>console.log );*/
+
+            const productos = document.createElement('div')
+            const dato = document.createElement('div')
+
+            let nombre = document.createElement('p')
+            nombre.appendChild(
+                document.createTextNode(`${user.name}`)
+            );
+
+            let empresa = document.createElement('p')
+            empresa.appendChild(
+                document.createTextNode(`${user.username}`)
+            );
+
+            let precio = document.createElement('p')
+            precio.appendChild(
+                document.createTextNode(`${user.email}`)
+            );
+
+            let mg = document.createElement('p')
+            mg.appendChild(
+                document.createTextNode(`${user.phone}`)
+            );
+            
+            dato.appendChild(nombre)
+            dato.appendChild(empresa)
+            dato.appendChild(precio)
+            dato.appendChild(mg)
+
+            productos.appendChild(dato)
+
+            dato.className = 'datos'
+            productos.className= 'producto'
+
+            
+
+            document.body.appendChild(productos);
+        })
+
+            
+    });
+    
