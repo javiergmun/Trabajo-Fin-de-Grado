c35f14acd3475159616fba893b13243a15a8dcd0
Daniel Castellote <daniel.castellote.b@gmail.com>
2022-06-08 20:53:06 +0200

    Filtrar comentarios y productos en perfiles

--- fetlix/series/static/js/perfil.js
+++ fetlix/series/static/js/perfil.js
@@ -1,40 +1,42 @@
 const API_URL ="http://127.0.0.1:8000";
 
-fetch(`${API_URL}/clientes/cliente`)
-        .then((response)=>response.json())
-        .then((response)=>{
-        response.forEach((pr)=>{
+const user_id = JSON.parse(document.getElementById('user_id').textContent);
 
-            fetch(`${API_URL}/posts/post/`)
+
+fetch(`${API_URL}/clientes/cliente/${user_id}`)
+        .then((response)=>response.json())
+        .then((r)=>{
+        
+            fetch(`${API_URL}/posts/post/cliente/${r.id}`)
             .then((response)=>response.json())
             .then((response)=>{
                 response.forEach((comentary)=>{
         
-                                
-                    let comentario = document.createElement('div')
-        
-                    let usuarioPost = document.createElement('p')
-                    usuarioPost.appendChild(
-                        document.createTextNode(`${comentary.producto}`)
-                    );
-        
-                    let valoracion = document.createElement('p')
-                    valoracion.appendChild(
-                        document.createTextNode(`${comentary.opinion}`)
-                    );
-        
-                    comentario.appendChild(usuarioPost)
-                    comentario.appendChild(valoracion)
-        
                     
-                    comentario.className='comentariosPerfil'
-                    document.body.appendChild(comentario);
+                        let comentario = document.createElement('div')
         
-                                
+                        let usuarioPost = document.createElement('p')
+                        usuarioPost.appendChild(
+                            document.createTextNode(`${comentary.producto}`)
+                        );
+            
+                        let valoracion = document.createElement('p')
+                        valoracion.appendChild(
+                            document.createTextNode(`${comentary.opinion}`)
+                        );
+            
+                        comentario.appendChild(usuarioPost)
+                        comentario.appendChild(valoracion)
+            
+                        
+                        comentario.className='comentariosPerfil'
+                        document.body.appendChild(comentario);
+                             
+                            
             });
         })
 
-        });
     })
+
 
 
---
+++ fetlix/series/static/js/perfil2.js
@@ -0,0 +1,105 @@
+const API_URL ="http://127.0.0.1:8000";
+
+fetch(`${API_URL}/productos/producto`)
+    .then((response)=>response.json())
+    .then((response)=>{
+        response.forEach((pro)=>{
+
+            const productos = document.createElement('div')
+            const idProductos = document.createAttribute('id')
+            idProductos.value='idProductos'
+            productos.setAttributeNode(idProductos)
+
+            const dato = document.createElement('div')
+            let comentarios = document.createElement('div')
+
+            //---------
+            let imagen = document.createElement('img')
+            let foto = document.createAttribute('src')
+
+            foto.value=`${pro.imagen}`
+            imagen.setAttributeNode(foto)
+
+            //let salto= document.createElement('br')
+
+            //---------
+            let nombre = document.createElement('p')
+            let nombreIcono = document.createElement('span')
+            nombreIcono.className='fas fa-box '
+            nombre.appendChild(nombreIcono)
+            nombre.appendChild(
+                document.createTextNode(" "+`${pro.nombre}`)
+            );
+
+            let empresa = document.createElement('p')
+            let empresaIcono = document.createElement('span')
+            empresaIcono.className='fas fa-user'
+            empresa.appendChild(empresaIcono)
+            empresa.appendChild(
+                document.createTextNode(" "+`${pro.empresa}`)
+            );
+
+
+            let precio = document.createElement('p')
+            let precioIcono = document.createElement('span')
+            precioIcono.className='fas fa-money-bill'
+            precio.appendChild(precioIcono)
+            precio.appendChild(
+                document.createTextNode(" "+`${pro.precio}`+" €")
+            );
+
+
+            let mg = document.createElement('i')
+            let idMg = document.createAttribute('id')
+            idMg.value='meGusta'
+            mg.setAttributeNode(idMg)
+            mg.appendChild(
+                document.createTextNode(" "+`${pro.num_likes}`)
+            );
+
+
+            let mgIcono = document.createElement('span')
+            let funcionMg = document.createAttribute('onclick')
+            let idIconoMg = document.createAttribute('id')
+            idIconoMg.value='iconoMeGusta'
+            funcionMg.value='doLike()'
+            mgIcono.setAttributeNode(funcionMg)
+            mgIcono.setAttributeNode(idIconoMg)
+            mgIcono.className='fas fa-heart '
+
+            //---------
+
+            let descripcion = document.createElement('p')
+            let idDescripcion= document.createAttribute('id')
+            let descripcionIcono = document.createElement('span')
+            idDescripcion.value='descripcion'
+            descripcionIcono.className='fas fa-scroll'
+            descripcion.appendChild(descripcionIcono)
+            descripcion.appendChild(
+                document.createTextNode(" "+pro.descripcion)
+            );
+            descripcion.setAttributeNode(idDescripcion)
+            descripcion.className='descripcion'
+            //---------
+
+            //dato.appendChild(nombreIcono)
+            dato.appendChild(nombre)
+            dato.appendChild(empresa)
+            dato.appendChild(precio)
+            dato.appendChild(mgIcono)
+            dato.appendChild(mg)
+
+            productos.appendChild(imagen)
+            productos.appendChild(dato)
+            productos.appendChild(descripcion)
+            
+            imagen.className = 'foto-producto'
+            dato.className = 'datos'
+            productos.className= 'producto'
+
+
+            document.body.appendChild(productos);
+        })
+
+
+});
